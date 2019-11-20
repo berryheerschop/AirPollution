ace9f948c9ec45e8b9bcd55530f7f694f17c4132
berryheerschop <berryheerschop@gmail.com>
2019-11-20 18:55:55 +0100

    Initial commit

---
+++ AirPollution.R
@@ -0,0 +1,17 @@
+# preparation step
+# Show working directory
+getwd()
+
+
+#unzip the zipfile containing the data
+
+unzip("rprog_data_specdata.zip")
+
+pollutantmean <- function(directory, pollutant, id=1:332){
+  
+  
+  
+  
+  
+  
+}
