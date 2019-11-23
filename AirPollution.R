1186cd79eea32b07eb47a5b4e7fdf0cdfe02892d
berryheerschop <berryheerschop@gmail.com>
2019-11-23 21:19:59 +0100

    new version

--- AirPollution.R
+++ AirPollution.R
@@ -1,31 +1,65 @@
-# preparation step
-# Show working directory
-getwd()
 
-
-#unzip the zipfile containing the data
-
-unzip("rprog_data_specdata.zip")
-
-pollutantmean <- function(directory=getwd(), pollutant, id=1:332){
+pollutantmean <- function(directory,pollutant,id=1:332) {
+  library(dplyr)
   
+  
+  # pollutantmean <- function(path=getwd(),column_name,id=1:5){
   # define the directory with the csv-files
-  data_dir <- file.path(getwd(),"specdata")
+  
+  data_dir <- file.path(getwd(),directory)
+  data_dir
+  
   
   #list the files in the directory
   files <- list.files(path=data_dir) 
+  files
+  
+  #id <- 1:7
+  #pollutant <- "nitrate"
   
   #select only the files specified in the id argument
   file_selection <- files[id]
+  file_selection
   dir_file_selection <- file.path(data_dir,file_selection)
+  dir_file_selection
   
-  i<- 1
-  for (i in length(dir_file_selection)){
+  
+  all_pollutant_values <-data.frame()
+
+  
+  num_files <- 1:length(id)
+
+
+  for (i in num_files) {
+
+    #select only the files specified in the id argument
+    file_selection <- files[id]
+    file_selection
+    dir_file_selection <- file.path(data_dir,file_selection)
+    dir_file_selection
     file_read <- read.csv(dir_file_selection[i])
-    file_read
+    #nitrate <-data.frame(file_read$nitrate)
+    pollutant_values <- select(file_read,pollutant)
+    
+    all_pollutant_values <- rbind(pollutant_values,all_pollutant_values)
+    goods <- !is.na(all_pollutant_values)
+    all_non_missing_pollutant_values <- all_pollutant_values[goods]
+    mean_pollutant_value <- mean(all_non_missing_pollutant_values)
+    mean_pollutant_value
+    
     i<- i+1
     
   }
+  print(length(id))
+  print("Analyzed files:")
+  print(file_selection)
+  print(paste("Pollutant mean for", pollutant, "is:",mean_pollutant_value))
   
 }
-pollutantmean(directory = getwd(),test,id=1:5)
+
+
+
+
+
+
+
